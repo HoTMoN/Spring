8c4aa8c5898dad99b0a2d0478405d8541e5af597
Dasol <sosgkwl@naver.com>
2021-10-07 22:55:21 +0900

    21_10_07 학습

---
+++ erp/src/main/java/main/IndexController.java
@@ -0,0 +1,22 @@
+package main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/index.do")
+	public String index() {
+		return "index";
+	}
+	
+//	@RequestMapping("/emp/login.do")
+//	public String loginPage() {
+//		return "login";
+//	}
+//	
+	@RequestMapping("/menu/board.do")
+	public String boardlist() {//db연동
+		return "board/list";
+	}
+}
